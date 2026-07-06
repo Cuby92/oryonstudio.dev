95cac1a6198dc436430845905e43c45255a0da36
Cuby92 <jakub.bartul@hotmail.com>
2026-07-06 11:19:36 +0200

    Optimize

--- app/src/components/Navbar/Sidebar/Sidebar.tsx
+++ app/src/components/Navbar/Sidebar/Sidebar.tsx
@@ -15,34 +15,15 @@ interface Props {
 function Sidebar({ open, headerHeight, setSidebarOpen } : Props) {
     const [activeLink, setActiveLink] = useState(0);
 
-    function setActive(link: number) {
-        switch (link) {
-            case 1:
-                setActiveLink(1);
-                break;
-            case 2:
-                setActiveLink(2);
-                break;
-            case 3:
-                setActiveLink(3);
-                break;
-            case 4:
-                setActiveLink(4);
-                break;
-            default:
-                setActiveLink(0);
-                break;
-        }
-    }
-
     return (
-        <div className={s.Sidebar} style={{ paddingTop: headerHeight }} onMouseLeave={ () => setActive(0) }>
+        <div className={s.Sidebar} style={{ paddingTop: headerHeight }} onMouseLeave={ () => setActiveLink(0) }>
             <div className={s.Section1}>
                 <nav className={s.Links}>
-                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActive(1)} active={activeLink === 1} href='/'        >Home</SidebarLink>
-                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActive(2)} active={activeLink === 2} href="/products">Products</SidebarLink>
-                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActive(3)} active={activeLink === 3} href="/about"   >About</SidebarLink>
-                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActive(4)} active={activeLink === 4} href="/contact" >Contact</SidebarLink>
+                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActiveLink(1)} active={activeLink === 1} href='/'          >Home          </SidebarLink>
+                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActiveLink(2)} active={activeLink === 2} href="/about"     >About Us      </SidebarLink>
+                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActiveLink(3)} active={activeLink === 3} href="/services"  >Our Services  </SidebarLink>
+                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActiveLink(4)} active={activeLink === 4} href="/experience">Our Experience</SidebarLink>
+                    <SidebarLink onClick={() => setSidebarOpen(false) } className={s.link} onMouseEnter={ () => setActiveLink(5)} active={activeLink === 5} href="/contact"   >Contact       </SidebarLink>
                 </nav>
             </div>
         </div>
