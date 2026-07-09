b748cd8969c609fa0fc0aeb4dfdfccca0549d813
Cuby92 <jakub.bartul@hotmail.com>
2026-07-09 20:43:17 +0200

    Implement in root layout

--- app/src/app/[locale]/layout.tsx
+++ app/src/app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-import { Children } from "@/utils/types";
-import "./globals.scss";
+import "../globals.scss";
 import Navbar from "@/components/Navbar/Navbar";
 import { Raleway, Poppins, Ubuntu_Sans_Mono, Google_Sans_Code } from "next/font/google";
 import { SpeedInsights } from '@vercel/speed-insights/next';
@@ -9,10 +8,10 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
-import { Locale } from '@/i18n/types';
+import type { Locale } from '@/i18n/types';
 
 export function generateStaticParams() {
-  return routing.locales.map(locale => { locale });
+  return routing.locales.map((locale) => ({ locale }));
 }
 
 const raleway = Raleway({
@@ -41,18 +40,20 @@ export const metadata: Metadata = {
   description: "We craft high-performance, visually stunning websites using Next.js. Tailored digital experiences for ambitious brands looking to stand out.",
 };
 
-async function RootLayout({ children, params }: { children: React.ReactNode, params: Promise<{ locale: Locale }> }) {
+async function RootLayout({ children, params }: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
   const { locale } = await params;
-  if (!routing.locales.includes(locale)) notFound();
 
-  const messages = await getMessages();
+  const isLocale = routing.locales.includes(locale as Locale);
+  if (!isLocale) notFound();
+
+  const messages = await getMessages({ locale });
 
   return (
-    <html lang="en" className={`${raleway.variable} ${poppins.variable} ${ubuntuMono.variable} ${googleCode.variable}`}>
+    <html lang={locale} className={`${raleway.variable} ${poppins.variable} ${ubuntuMono.variable} ${googleCode.variable}`}>
       <body>
         <NextIntlClientProvider messages={messages}>
           <Navbar />
-          { children }
+          {children}
         </NextIntlClientProvider>
         <SpeedInsights />
         <Analytics />
