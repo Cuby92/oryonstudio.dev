d0e0da9e26e1147d6f667dd5fdc226439b446b4f
Cuby92 <jakub.bartul@hotmail.com>
2026-07-13 20:40:17 +0200

    Change aurora height

--- app/src/components/shaders/LiquidAurora.tsx
+++ app/src/components/shaders/LiquidAurora.tsx
@@ -9,15 +9,16 @@ function LiquidAurora() {
                 position: 'absolute',
                 width: '110vw',
                 height: '110vh',
-                top: '10%',
+                top: '50%',
                 left: '50%',
-                transform: 'translateX(-50%)',
+                transform: 'translate(-50%, -50%)',
                 zIndex: -1
             }}
         >
             <Aurora
                 colorA='#00f'
                 colorB='#44f'
+                height={190}
             />
             <ChromaFlow
                 baseColor='#00f'
