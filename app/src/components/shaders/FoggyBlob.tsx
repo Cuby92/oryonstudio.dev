4283fb88294c6bc592e0a9699692c409dac5b34a
Cuby92 <jakub.bartul@hotmail.com>
2026-07-13 21:55:55 +0200

    Create FoggyBlob shader component

---
+++ app/src/components/shaders/FoggyBlob.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Shader, Blob, Fog } from 'shaders/react';
+
+function FoggyBlob() {
+    return (
+        <Shader 
+            style={{
+                position: 'absolute',
+                width: '110vw',
+                height: '110vh',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                zIndex: -1
+            }}
+        >
+            <Blob
+                colorA='#00f'
+                colorB='#33f'
+            />
+            <Fog
+                colorA='#00fa'
+                colorB='#0000'
+            />
+        </Shader>
+    );
+}
+
+export default FoggyBlob;
