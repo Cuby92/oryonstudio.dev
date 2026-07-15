62e14b87ab465f8c0a1c923553082fa5b2d251ea
Cuby92 <jakub.bartul@hotmail.com>
2026-07-15 18:03:35 +0200

    Delete empty slide

--- app/src/app/[locale]/page.tsx
+++ app/src/app/[locale]/page.tsx
@@ -17,9 +17,6 @@ function Home() {
           <FoggyBlob />
           <h1>{ t('page under construction') } <span className={s.pointer}>█</span></h1>
         </Slide>
-        <Slide>
-          <p></p>
-        </Slide>
       </main>
       <Footer />
     </ScrollSmootherWrapper>
