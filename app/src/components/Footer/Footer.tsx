bd99bc9b9d42952aa47810f33f7854f0f1aab3f1
Cuby92 <jakub.bartul@hotmail.com>
2026-07-17 20:46:18 +0200

    Add content

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -2,25 +2,38 @@ import styles from "./Footer.module.scss";
 import { LinkTemplate } from '@/utils/types';
 import CypherLink from '@/components/links/cypher';
 import { useTranslations } from 'next-intl';
+import LogoDraw from "@/components/LogoDraw";
+import Link from 'next/link';
 
 const s = styles;
 
 function Footer() {
-    const t = useTranslations('global.links');
+    const t = useTranslations('global');
 
     const links: LinkTemplate[] = [
-        { href: '/',           label: t('home')       },
-        { href: '/about',      label: t('about')      },
-        { href: '/experience', label: t('experience') },
-        { href: '/services',   label: t('services')   },
-        { href: '/contact',    label: t('contact')    }
+        { href: '/',           label: t('links.home')       },
+        { href: '/about',      label: t('links.about')      },
+        { href: '/experience', label: t('links.experience') },
+        { href: '/services',   label: t('links.services')   },
+        { href: '/contact',    label: t('links.contact')    }
     ];
 
     return (
         <footer className={s.Footer}>
+            <div className={s.info}>
+                <Link href="/">
+                    <LogoDraw active={true} />
+                </Link>
+                <p>The cosmic standard for standout brands.</p>
+                <p><div className={s.indicator}></div>Available for select Q3/Q4 projects.</p>
+            </div>
+
             <nav>
                 { links.map(link => <CypherLink className={s.link} label={link.label} href={link.href} key={links.indexOf(link)} />)}
             </nav>
+
+            <p className={s.techStack}>{ t('footer.humanTouch.designed_and_engineered_by') } <a target='_blank'>{ t('footer.humanTouch.powered_by_nextjs') }</a></p>
+            <p className={s.copyright}>{ t('footer.copyright') }</p>
         </footer>
     );
 }
