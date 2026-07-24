d635bd36323ae0ab1082ee18db5e4da2057e505e
Cuby92 <jakub.bartul@hotmail.com>
2026-07-24 08:38:14 +0200

    Update page.tsx

--- app/src/app/[locale]/contact/page.tsx
+++ app/src/app/[locale]/contact/page.tsx
@@ -1,7 +1,6 @@
 import styles from './page.module.scss';
 import Slide from '@/components/Slide/Slide';
 import { useTranslations } from 'next-intl';
-import FlareBtn from '@/components/links/FlareBtn';
 import LiquidPlasma from '@/components/shaders/LiquidPlasma';
 import { HorizontalSection, HorizontalPanel } from '@/components/horizontal-scroll/horizontal-scroll';
 import Image from 'next/image';
@@ -9,6 +8,10 @@ import q1BgText from '@/../public/dotted-kihim-text/pages.svg';
 import q2BgText from '@/../public/dotted-kihim-text/type.svg';
 import q3BgText from '@/../public/dotted-kihim-text/creativity.svg';
 import q4BgText from '@/../public/dotted-kihim-text/you.svg';
+import summaryBgText from '@/../public/dotted-kihim-text/summary.svg';
+import Web3Forms from '@/components/forms/Web3Forms';
+import Options from '@/components/forms/Options';
+import TextField from '@/components/forms/TextField';
 
 const s = styles;
 
@@ -25,63 +28,89 @@ function Contact() {
                 </div>
             </Slide>
 
-            <HorizontalSection className={s.form} id="form">
-                <HorizontalPanel>
-                    <div className={`${s.question} ${s.q1}`}>
-                        <Image
-                            className={s.bgText}
-                            src={q1BgText}
-                            alt="pages"
-                        />
-                        
-                        <div className={s.blueRect} />
+            <Web3Forms>
+                <HorizontalSection className={s.form} id="form">
+                    <HorizontalPanel>
+                        <div className={`${s.question} ${s.q1}`}>
+                            <Image
+                                className={s.bgText}
+                                src={q1BgText}
+                                alt="pages"
+                            />
+                            
+                            <div className={s.blueRect} />
 
-                        <div className={s.modal}>
-                            <h2>How many pages do you need on your website?</h2>
+                            <div className={s.modal}>
+                                <h2>How many pages do you need on your website?</h2>
+                                <p>If you don't know the exact quantity or you need advice - don't worry! Just click "I don't know". We will assist you with the decision.</p>
+                                
+                                <div className={s.form}>
+                                    <Options options={['1', '2-5', '6-10', '>10', "I don't know"]}>
+                                        <TextField name="custom" placeholder="Custom" />
+                                    </Options>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </HorizontalPanel>
+                    </HorizontalPanel>
 
-                <HorizontalPanel>
-                    <div className={`${s.question} ${s.q2}`}>
-                        <Image
-                            className={s.bgText}
-                            src={q2BgText}
-                            alt="pages"
-                        />
+                    <HorizontalPanel>
+                        <div className={`${s.question} ${s.q2}`}>
+                            <Image
+                                className={s.bgText}
+                                src={q2BgText}
+                                alt="type"
+                            />
 
-                        <div className={`${s.blueRect} ${s.br1}`} />
-                        <div className={`${s.blueRect} ${s.br2}`} />
+                            <div className={`${s.blueRect} ${s.br1}`} />
+                            <div className={`${s.blueRect} ${s.br2}`} />
 
-                        <div className={s.modal}>
-                            <h2>What type of website are you looking for?</h2>
+                            <div className={s.modal}>
+                                <h2>What type of website are you looking for?</h2>
+                            </div>
                         </div>
-                    </div>
-                </HorizontalPanel>
+                    </HorizontalPanel>
 
-                <HorizontalPanel>
-                    <div className={`${s.question} ${s.q3}`}>
-                        <Image
-                            className={s.bgText}
-                            src={q3BgText}
-                            alt="pages"
-                        />
+                    <HorizontalPanel>
+                        <div className={`${s.question} ${s.q3}`}>
+                            <Image
+                                className={s.bgText}
+                                src={q3BgText}
+                                alt="creativity"
+                            />
 
-                        <div className={`${s.blueRect} ${s.br1}`} />
-                        <div className={`${s.blueRect} ${s.br2}`} />
+                            <div className={`${s.blueRect} ${s.br1}`} />
+                            <div className={`${s.blueRect} ${s.br2}`} />
 
-                        <div className={s.modal}>
-                            <h2>How creative do you want it to be?</h2>
+                            <div className={s.modal}>
+                                <h2>How creative do you want it to be?</h2>
+                            </div>
                         </div>
-                    </div>
-                </HorizontalPanel>
+                    </HorizontalPanel>
 
-                <HorizontalPanel>
-                    <div className={`${s.question} ${s.q4}`}>
+                    <HorizontalPanel>
+                        <div className={`${s.question} ${s.q4}`}>
+                            <Image
+                                className={s.bgText}
+                                src={q4BgText}
+                                alt="you"
+                            />
+
+                            <div className={`${s.blueRect} ${s.br1}`} />
+                            <div className={`${s.blueRect} ${s.br2}`} />
+
+                            <div className={s.modal}>
+                                <h2>Your information</h2>
+                            </div>
+                        </div>
+                    </HorizontalPanel>
+                </HorizontalSection>
+
+                <Slide className={s.summary}>
+                    <div className={`${s.question}`}>
                         <Image
                             className={s.bgText}
-                            src={q4BgText}
-                            alt="pages"
+                            src={summaryBgText}
+                            alt="summary"
                         />
 
                         <div className={`${s.blueRect} ${s.br1}`} />
@@ -91,8 +120,8 @@ function Contact() {
                             <h2>Your information</h2>
                         </div>
                     </div>
-                </HorizontalPanel>
-            </HorizontalSection>
+                </Slide>
+            </Web3Forms>
         </>
     );
 }
